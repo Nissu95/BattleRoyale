6185f72bfe4bdb51d994abddc6e3e761d7aff5b8
Pehuen <pehuen-testa@hotmail.com>
2018-11-28 16:23:24 -0300

    +Player movement, player assets, shot, networking

---
+++ BattleRoyale/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    [SerializeField] int damage;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var hit = collision.gameObject;
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
