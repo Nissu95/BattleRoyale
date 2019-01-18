2123959fd66cd1729924129f119819444ef63cc0
Nissu <nicosus95@gmail.com>
2019-01-18 13:26:30 -0300

    +Win when only 1 player remains

---
+++ BattleRoyale/Assets/Scripts/CantPlayersTxt.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class CantPlayersTxt : MonoBehaviour
+{
+    [SerializeField] Text cantPlayersTxt;
+
+    private void Update()
+    {
+        cantPlayersTxt.text = "Players: " + GameManager.instance.cantPlayers;
+    }
+
+}
