fa4755d55fb456c80c906e41023d955023440d2e
Nissu <nicosus95@gmail.com>
2019-01-18 12:10:17 -0300

    +Limit of players

---
+++ BattleRoyale/Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        
+        else if (instance != this)
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+
+        //NetworkManager.singleton.maxConnections = 3;
+    }
+}
